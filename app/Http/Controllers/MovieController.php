ce057569412afabe5b51b6ff390a68c10a62f796
Michael Vreeken <mike@vreeken.dev>
2019-06-10 13:49:08 -0700

    Change movie json files to be named after movie id

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -21,23 +21,14 @@ class MovieController extends Controller
 	/**
 	 * Show an individual movie's subtitle data
 	 *
-	 * We get the movie's id from the url, then use that to get the movie's tt_id, which is used to get the movies json
-	 * TODO Perhaps we should rename the json files to just <movie_id>.json instead of <tt_id>.json
+	 * We get the movie's id from the url, then use that to get the movie's json data from storage
 	 *
 	 * @param Movie $movie
 	 * @return \Illuminate\Http\JsonResponse
 	 */
 	public function show(Movie $movie) {
-		/*
-		 We used to use url with tt_id, but now use movie_id
-		//tt0076759
-		if (!preg_match('/t{2}\d{7}/', $id)) {
-			return response()->json(['error'=>'error', 'msg'=>'Invalid movie id. Must use format: tt######'], 400);
-		}
-		*/
-
 		try {
-			$json = Storage::disk('local')->get('json/' . $movie->tt_id . '_formatted.json');
+			$json = Storage::disk('local')->get('json/' . $movie->id . '.json');
 		}
 		catch (\Exception $e) {
 			return response()->json(['error'=>'error', 'msg'=>'No movie data found for: '.$movie->title.'(id: '.$movie->id], 404);
